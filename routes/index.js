39110d2fe8e832679ff96e9bc561fb328befe050
Nazarenko <nazarenko@MacBook-Pro.local>
2024-11-22 19:28:50 +0200

    add user routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const { UserController } = require('../controllers');
 
 const uploadDestination = 'uploads'
 
@@ -18,8 +19,10 @@ router.get('/', (req, res) => {
   res.send('home page')
 })
 
-router.get('/register', (req, res) => {
-  res.send('register')
-})
+router.post('/register', UserController.register)
+router.post('/login', UserController.login)
+router.get('/current', UserController.current)
+router.get('/users/:id', UserController.login)
+router.put('/users/:id', UserController.updateUser)
 
 module.exports = router;
