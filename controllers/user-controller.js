208ca7cf507a19c3e00ab4bf1fc4c69b2e98578f
Nazarenko <nazarenko@MacBook-Pro.local>
2025-01-17 21:19:02 +0200

    create register and login controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,88 @@
+const { prisma } = require("../prisma/prisma-client");
+const bcrypt = require('bcrypt');
+const Jdenticon = require('jdenticon');
+const path = require('path');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+
 const UserController = {
-	register: async () => { },
-	login: async () => { },
-	getUserById: async () => { },
-	updateUser: async () => { },
-	current: async () => { },
+	register: async (req, res) => {
+		const { email, password, name } = req.body
+
+		if (!email || !password || !name) {
+			return res.status(400).json({ error: "All filds are required" })
+		}
+
+		try {
+			const existingUser = await prisma.user.findUnique(({ where: { email } }))
+
+			if (existingUser) {
+				return res.status(400).json({ error: "There is already user with this email" })
+			}
+
+			const hashedPassword = await bcrypt.hash(password, 10);
+
+			//creat user icon
+			const png = Jdenticon.toPng(name, 200);
+			const avatarName = `${name}_${Date.now()}.png`;
+			const avatarPath = path.join(__dirname, '../uploads', avatarName);
+			fs.writeFileSync(avatarPath, png)
+
+
+			const user = await prisma.user.create({
+				data: {
+					email,
+					password: hashedPassword,
+					name,
+					avatarUrl: `/uploads/${avatarPath}`
+				}
+			})
+
+			res.json(user)
+
+		} catch (error) {
+			console.error('Error in register', error);
+			res.status(500).join({ error: "Internal server error" });
+		}
+	},
+	login: async (req, res) => {
+		const { email, password } = req.body;
+
+		if (!email || !password) {
+			return res.status(400).json({ error: "All fields are required" })
+		}
+
+		try {
+			const user = await prisma.user.findUnique({ where: { email } })
+
+			if (!user) {
+				return res.status(400).json({ error: "Login or password is incorrect" })
+			}
+
+			const valid = await bcrypt.compare(password, user.password);
+
+			if (!valid) {
+				return res.status(400).json({ error: "Login or password is incorrect" })
+			}
+
+			const token = jwt.sign(({ userId: user.id }), process.env.SECRET_KEY);
+
+			res.json({ token })
+
+		} catch (error) {
+			console.error("Login error", error);
+			res.status(500).join({ error: "Internal server error" });
+		}
+	},
+	getUserById: async (req, res) => {
+		res.send('getUserById')
+	},
+	updateUser: async (req, res) => {
+		res.send('updateUser')
+	},
+	current: async (req, res) => {
+		res.send('current')
+	},
 }
 
 module.exports = UserController;
